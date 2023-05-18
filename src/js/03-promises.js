8b0bd0aeb9ecbde0818b8ceddd4887fe9e4a4749
AdamSkubida <adasiow.s@gmail.com>
2023-05-18 23:44:40 +0200

    newPromise/future

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,16 +1,21 @@
 'use strict';
 
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const form = document.querySelector('.form');
 const firstDelay = document.querySelector('[name="delay"]');
 const delayStep = document.querySelector('[name="step"]');
 const promisesAmount = document.querySelector('[name="amount"]');
-const submitBtn = document.querySelector('button');
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      // Fulfill
+      resolve({ position, delay });
+    } else {
+      // Reject
+      reject({ position, delay });
+    }
+  }, delay);
 }
