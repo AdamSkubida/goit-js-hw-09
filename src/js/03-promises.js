3d126538966a735d397e5779909c0df99c2c9195
AdamSkubida <adasiow.s@gmail.com>
2023-05-18 21:10:51 +0200

    promises/vars/future

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,11 @@
+'use strict';
+
+const form = document.querySelector('.form');
+const firstDelay = document.querySelector('[name="delay"]');
+const delayStep = document.querySelector('[name="step"]');
+const promisesAmount = document.querySelector('[name="amount"]');
+const submitBtn = document.querySelector('button');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
